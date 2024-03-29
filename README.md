ca3efcfa36ef4dd68873b848da2b7ed86bb3afff
lyq <705194673@qq.com>
2024-03-29 09:32:13 +0800

    ✨feat(all): 优化 README 文档；改变图标；在取消激活插件时清理背景

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Background
+
+改变设置后需要重启生效
+
+## 定义每个区域的背景图片
+
+开启背景图片功能，选中 `partition` 模式
+
+![alt text](readmeImgs/image.png)
+
+配置各个区域的背景图片路径（本地地址，base64或者网页地址）和透明度。
+
+![alt text](readmeImgs/image-4.png)
+
+结果展示：
+
+![alt text](PixPin_2024-03-29_08-59-47.png)
+
+## 定义全屏背景图片
+
+开启背景图片功能，选中 `fullScreen` 模式
+
+![alt text](readmeImgs/image-1.png)
+
+配置背景图片路径（本地地址，base64或者网页地址）和透明度。
+
+![alt text](readmeImgs/image-3.png)
+
+结果展示：
+
+![alt text](readmeImgs/image-2.png)
--- extension.js
+++ extension.js
@@ -5,72 +5,86 @@ const fs = require('fs');
 const styleStartLabel = 'LifeQuality-background-start';
 const styleEndLabel = 'LifeQuality-background-end';
 const styleRegExp = new RegExp(`/\\* ${styleStartLabel} \\*/[\\s\\S]*/\\* ${styleEndLabel} \\*/`, 'g');
-const partNames = ['titlebar','banner','activitybar','sidebar','editor','panel','auxiliarybar','statusbar']
+const partNames = ['titlebar', 'banner', 'activitybar', 'sidebar', 'editor', 'panel', 'auxiliarybar', 'statusbar'];
 const styleFilePath = path.join(path.dirname(require.main.filename), 'vs', 'workbench', `workbench.${vscode.env.appHost}.main.css`);
 
 function getBackgroundStyle(config) {
-    if (config.showType == 'fullScreen') {
-        const { imgUrl, opacity } = config.fullScreen;
-        return `/* ${styleStartLabel} */
-            body::after {
-                content: '';
-                position: fixed;
-                top: 0;
-                left: 0;
-                display: block;
-                width: 100%;
-                height: 100%;
-                background: url(${imgUrl}) 50% 50%/cover;
-                opacity: ${opacity};
-                pointer-events: none;
-                z-index: 999;
-            }
-        /* ${styleEndLabel} */`.replace(/\s+/g, ' ');
-    } else if(config.showType == 'partition') {
-        const partitionStyle =  partNames.reduce((partitionStyle, partName) => {
-            console.log(config,partName, config[partName], config[partName].imgUrl);
-            return partitionStyle + (config[partName].imgUrl ? `[id='workbench.parts.${partName}']::after {
-                content: '';
-                position: absolute;
-                top: 0;
-                left: 0;
-                display: block;
-                width: 100%;
-                height: 100%;
-                background: url(${config[partName].imgUrl}) 50% 50%/cover;
-                opacity: ${config[partName].opacity};
-                pointer-events: none;
-                z-index: 999;
-            }` : '');
-        }, '');
-        return `/* ${styleStartLabel} */
-            ${partitionStyle}
-        /* ${styleEndLabel} */`.replace(/\s+/g, ' ');
-    }
+  if (config.showType == 'fullScreen') {
+    const { imgUrl, opacity } = config.fullScreen;
+    return `/* ${styleStartLabel} */
+      body::after {
+        content: '';
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 100%;
+        height: 100%;
+        background: url(${imgUrl}) 50% 50%/cover;
+        opacity: ${opacity};
+        pointer-events: none;
+        z-index: 999;
+      }
+    /* ${styleEndLabel} */`.replace(/\s+/g, ' ');
+  } else if (config.showType == 'partition') {
+    const partitionStyle = partNames.reduce((partitionStyle, partName) => {
+      console.log(config, partName, config[partName], config[partName].imgUrl);
+      return (
+        partitionStyle +
+        (config[partName].imgUrl
+          ? `[id='workbench.parts.${partName}']::after {
+              content: '';
+              position: absolute;
+              top: 0;
+              left: 0;
+              display: block;
+              width: 100%;
+              height: 100%;
+              background: url(${config[partName].imgUrl}) 50% 50%/cover;
+              opacity: ${config[partName].opacity};
+              pointer-events: none;
+              z-index: 999;
+            }`
+          : '')
+      );
+    }, '');
+    return `/* ${styleStartLabel} */
+      ${partitionStyle}
+    /* ${styleEndLabel} */`.replace(/\s+/g, ' ');
+  }
 }
 
 function replaceStyle(config) {
-    const styleData = fs.readFileSync(styleFilePath, { encoding: 'utf8', flag: 'r' }).toString();
-    const newStyleData = styleData.replace(styleRegExp, '') + getBackgroundStyle(config);
-    fs.writeFileSync(styleFilePath, newStyleData, {});
+  const styleData = fs.readFileSync(styleFilePath, { encoding: 'utf8', flag: 'r' }).toString();
+  const newStyleData = styleData.replace(styleRegExp, '') + getBackgroundStyle(config);
+  fs.writeFileSync(styleFilePath, newStyleData, {});
 }
 
 function clearStyle() {
-    const styleData = fs.readFileSync(styleFilePath, { encoding: 'utf8', flag: 'r' }).toString();
-    const newStyleData = styleData.replace(styleRegExp, '');
-    fs.writeFileSync(styleFilePath, newStyleData, {});
+  const styleData = fs.readFileSync(styleFilePath, { encoding: 'utf8', flag: 'r' }).toString();
+  const newStyleData = styleData.replace(styleRegExp, '');
+  fs.writeFileSync(styleFilePath, newStyleData, {});
+}
+
+function handleConfig() {
+  const config = vscode.workspace.getConfiguration('se').get('background');
+  if (config.isOpened) {
+    replaceStyle(config);
+  } else {
+    clearStyle();
+  }
 }
 
 module.exports = {
-    activate(context) {
-        vscode.workspace.onDidChangeConfiguration(() => {
-            const config = vscode.workspace.getConfiguration('se').get('background');
-            if (config.isOpened) {
-                replaceStyle(config);
-            } else {
-                clearStyle();
-            }
-        });
-    },
-    deactivate() {},
+  activate(context) {
+    context.subscriptions.push(
+      vscode.workspace.onDidChangeConfiguration(() => {
+        handleConfig();
+      })
+    );
+    handleConfig();
+  },
+  deactivate() {
+    clearStyle();
+  },
 };
